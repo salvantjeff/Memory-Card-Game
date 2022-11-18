226c2b1691f0f1d2dcb03854531f0d74a8853ec2
Jeff-Salvant <salvant3jeff@gmail.com>
2022-11-18 17:06:12 -0500

    Create header component file

---
+++ memory-card/src/components/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Header(props) {
+    return (
+        <header>
+            <div className="title">
+                <h1>Memory Game</h1>
+            </div>
+        </header>
+    );
+};
