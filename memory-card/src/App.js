9bfc3f5f4598fbb63f5df793c3aa7a674bccca8f
Jeff-Salvant <salvant3jeff@gmail.com>
2022-11-18 17:52:15 -0500

    Invoke generateBuildOrder using card description list

--- memory-card/src/App.js
+++ memory-card/src/App.js
@@ -1,11 +1,15 @@
 import './App.css';
 import Game from "./Game";
+import generateBuildOrder from "./generateBuildOrder";
 
 const gameInit = new Game(0, 0);
+const cardDescriptions = ['first', 'second', 'third', 'fourth'];
+const initBuildOrder = generateBuildOrder(cardDescriptions.length);
+
 function App() {
   return (
     <div className="App">
-
+      
     </div>
   );
 }
