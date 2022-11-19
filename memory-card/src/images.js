4586b630acddfd79d593ac8ae4253794070a218f
Jeff-Salvant <salvant3jeff@gmail.com>
2022-11-18 21:42:05 -0500

    Convert images from array to object

--- memory-card/src/images.js
+++ memory-card/src/images.js
@@ -1,14 +1,13 @@
-const images = [
-    "https://cdn.vox-cdn.com/thumbor/Afwbr-tBGzQt1F_pZ94bN4JBkSQ=/1400x1400/filters:format(jpeg)/cdn.vox-cdn.com/uploads/chorus_asset/file/24013044/1422680460.jpg",
-    "https://i.ytimg.com/vi/3lDsblk1_II/maxresdefault.jpg",
-    "https://www.mmaindia.com/wp-content/uploads/2019/10/073_Israel_Adesanya_x_Derek_Brunson.0.0.jpg",
-    "https://sportshub.cbsistatic.com/i/r/2021/04/25/6f991244-ddea-4ba7-b737-ac5b20b1035c/thumbnail/1200x675/3875529dde39b7bfc1fe3c4c9d3f380d/kamaru-usman-scream.jpg",
-    "https://images.complex.com/complex/images/c_fill,dpr_auto,f_auto,q_90,w_1400/fl_lossy,pg_1/yuyvoyaom5tdnlfzqaki/conor-mcgregor-khabib-nurmagomedov",
-    "https://cdn.vox-cdn.com/thumbor/SMw_GsowcxrxEwQ3GIzFp-5x2hU=/1400x1400/filters:format(jpeg)/cdn.vox-cdn.com/uploads/chorus_asset/file/23976496/DC_8862.jpg",
-    "https://i.dailymail.co.uk/1s/2022/01/18/19/53084447-10415683-image-a-37_1642535610971.jpg",
-    "https://cdn.vox-cdn.com/thumbor/0s22Ie_9U5lZAVr43mPRYIZHmVI=/0x0:4437x3246/1200x800/filters:focal(2709x592:3417x1300)/cdn.vox-cdn.com/uploads/chorus_image/image/70865423/1396020773.0.jpg",
-    "https://i.insider.com/5d26ffb8a17d6c0ec8266343?width=1136&format=jpeg",
-    "https://www.sportszion.com/wp-content/uploads/2022/05/jj.webp",
-];
-
+const images = {
+    'card1': "https://cdn.vox-cdn.com/thumbor/Afwbr-tBGzQt1F_pZ94bN4JBkSQ=/1400x1400/filters:format(jpeg)/cdn.vox-cdn.com/uploads/chorus_asset/file/24013044/1422680460.jpg",
+    'card2':  "https://i.ytimg.com/vi/3lDsblk1_II/maxresdefault.jpg",
+    'card3':  "https://www.mmaindia.com/wp-content/uploads/2019/10/073_Israel_Adesanya_x_Derek_Brunson.0.0.jpg",
+    'card4': "https://sportshub.cbsistatic.com/i/r/2021/04/25/6f991244-ddea-4ba7-b737-ac5b20b1035c/thumbnail/1200x675/3875529dde39b7bfc1fe3c4c9d3f380d/kamaru-usman-scream.jpg",
+    'card5': "https://images.complex.com/complex/images/c_fill,dpr_auto,f_auto,q_90,w_1400/fl_lossy,pg_1/yuyvoyaom5tdnlfzqaki/conor-mcgregor-khabib-nurmagomedov",
+    'card6': "https://cdn.vox-cdn.com/thumbor/SMw_GsowcxrxEwQ3GIzFp-5x2hU=/1400x1400/filters:format(jpeg)/cdn.vox-cdn.com/uploads/chorus_asset/file/23976496/DC_8862.jpg",
+    'card7': "https://i.dailymail.co.uk/1s/2022/01/18/19/53084447-10415683-image-a-37_1642535610971.jpg",
+    'card8': "https://cdn.vox-cdn.com/thumbor/0s22Ie_9U5lZAVr43mPRYIZHmVI=/0x0:4437x3246/1200x800/filters:focal(2709x592:3417x1300)/cdn.vox-cdn.com/uploads/chorus_image/image/70865423/1396020773.0.jpg",
+    'card9': "https://i.insider.com/5d26ffb8a17d6c0ec8266343?width=1136&format=jpeg",
+    'card10': "https://www.sportszion.com/wp-content/uploads/2022/05/jj.webp",
+}
 export default images;
