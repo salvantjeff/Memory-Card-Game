ddb156bf6c88869cdeeeaae4228cc4c574fe36fa
Jeff-Salvant <salvant3jeff@gmail.com>
2022-11-18 21:56:32 -0500

    Add game description

--- memory-card/src/components/Header.js
+++ memory-card/src/components/Header.js
@@ -6,6 +6,7 @@ export default function Header(props) {
         <header>
             <div className="title">
                 <h1>UFC Legends Memory Game</h1>
+                <h4>Earn points by clicking on an image but you can only click once!</h4>
             </div>
             <ScoreBoard 
                 currentScore={props.currentScore}
